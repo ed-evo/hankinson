2cb9d42ae48cf812428a9577c1600d2a80c607de
Lingyong Sun <lingyong.sun@gmail.com>
2026-07-24 20:26:24 +0200

    feat(UI): add basic chart

--- ui/src/pages/index.vue
+++ ui/src/pages/index.vue
@@ -12,20 +12,48 @@ meta:
             <v-icon>mdi-pencil</v-icon>{{ quizStore.capitoliSelezionati?.length }}
         </v-btn>
         <hr />
+        <v-pie
+            :items="pieChartData"
+            inner-cut="75"
+            hide-slice
+        >
+            <template v-slot:center="{ total }">
+                <div class="text-center">
+                    <v-chip prepend-icon="mdi-check" color="green"><strong>{{ quesitiStats?.corrette }}</strong></v-chip>
+                    <v-chip prepend-icon="mdi-close" color="red">{{ quesitiStats?.sbagliate }}</v-chip>
+                </div>
+            </template>
+        </v-pie>
         {{ quesitiStats }}
     </v-sheet>
 </template>
 
 <script lang="ts" setup>
+import { VPie } from 'vuetify/labs/VPie'
 import { getQuesitiStats, type QuesitiBasicStats } from '@/services/hankinson';
 import { useAppStore } from '@/stores/app';
 import { useQuizStore } from '@/stores/quiz';
-import { onMounted, ref } from 'vue';
+import { computed, onMounted, ref } from 'vue';
 
 const quizStore = useQuizStore()
 const appStore = useAppStore()
 
 const quesitiStats = ref<QuesitiBasicStats>()
+
+const pieChartData = computed(() => {
+    if (!quesitiStats.value) {
+        return undefined
+    }
+    const {
+        corrette,
+        sbagliate,
+        non_date
+    } = quesitiStats.value
+    return [
+        { key: 'corrette', title: 'Corrette', value: corrette, color: 'green' },
+        { key: 'sbagliate', title: 'Sbagliate', value: sbagliate, color: 'red' },
+    ]
+})
 
 onMounted(async () => {
     quesitiStats.value = await getQuesitiStats()
