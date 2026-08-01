e8380c2637cf119083eac9de25d1c70a191e4b91
Lingyong Sun <lingyong.sun@gmail.com>
2026-08-01 12:24:19 +0200

    feat(server): init Esame parziale

--- server/internal/api/quiz/esami/parziale.go
+++ server/internal/api/quiz/esami/parziale.go
@@ -1,10 +1,13 @@
 package esami_api
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/ed-evo/hankinson/server/ent"
 	"github.com/ed-evo/hankinson/server/ent/esame"
+	api_middlewares "github.com/ed-evo/hankinson/server/internal/api/middlewares"
+	"github.com/ed-evo/hankinson/server/internal/orm"
 	"github.com/ed-evo/hankinson/server/internal/utils"
 	"github.com/ed-evo/hankinson/server/pkg/api/api_errors"
 	"github.com/go-chi/chi/v5"
@@ -16,7 +19,7 @@ func newParzialiRouter(db *ent.Client) chi.Router {
 
 	parzialiRouter.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		all, err := db.Esame.Query().
-			Where(esame.TipoEQ(esame.TipoAperto)).
+			Where(esame.TipoEQ(esame.TipoParziale)).
 			All(r.Context())
 		if err != nil {
 			render.Render(w, r, api_errors.ErrInternal(err))
@@ -24,6 +27,7 @@ func newParzialiRouter(db *ent.Client) chi.Router {
 		}
 		render.JSON(w, r, all)
 	})
+	parzialiRouter.Put("/", createParziale(db))
 
 	return parzialiRouter
 }
@@ -44,6 +48,52 @@ func (p *EsameParzialeBody) Bind(r *http.Request) error {
 
 func createParziale(db *ent.Client) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		body := &EsameParzialeBody{}
+		if err := render.Bind(r, body); err != nil {
+			render.Render(w, r, api_errors.ErrInvalidRequest(err))
+			return
+		}
+		log.Printf("Request %v", body)
 
+		domandeIDs, err := orm.RandomDomandeIds(body.Capitoli, body.NumeroQuesiti)
+		if err != nil {
+			render.Render(w, r, api_errors.ErrInternal(err))
+			return
+		}
+
+		log.Printf("Domande IDs: %v", domandeIDs)
+
+		user := api_middlewares.GetUser(r)
+
+		ctx := r.Context()
+
+		esame, err := db.Esame.Create().
+			SetTipo(esame.TipoParziale).
+			SetNumeroQuesiti(body.NumeroQuesiti).
+			SetUtenteID(string(*user)).
+			SetMaxErrori(body.MaxErrori).
+			SetMinutiDisponibili(body.MinutiDisponibili).
+			Save(ctx)
+		if err != nil {
+			render.Render(w, r, api_errors.ErrInternal(err))
+			return
+		}
+
+		var quesiti []*ent.QuesitoEsame
+		for _, domandaID := range domandeIDs {
+			q, err := db.QuesitoEsame.Create().
+				SetEsameID(esame.ID).
+				SetDomandaOriginaleID(domandaID).
+				Save(ctx)
+			if err != nil {
+				render.Render(w, r, api_errors.ErrInternal(err))
+				return
+			}
+			quesiti = append(quesiti, q)
+		}
+		render.JSON(w, r, map[string]interface{}{
+			"esame":   esame,
+			"quesiti": quesiti,
+		})
 	}
 }
